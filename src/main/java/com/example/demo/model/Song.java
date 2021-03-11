5a4ffe0ad0517e08ccbf816d4a5876fedcb709c7
minhcmc <minh@wescheduleit.co.uk>
2021-03-11 10:50:21 +0700

    Test

--- src/main/java/com/example/demo/model/Song.java
+++ src/main/java/com/example/demo/model/Song.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
-@JsonIgnoreProperties(value = {"album", "banner", "types", "playlists"}, allowSetters = true)
+@JsonIgnoreProperties(value = {"album", "banner", "types", "playlists"})
 public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
