d887d5485393a32bf87d5b1bc113b15fb52a3528
minhcmc <minh@wescheduleit.co.uk>
2021-03-11 10:10:36 +0700

    Thử JsonIgnoreProperties

--- src/main/java/com/example/demo/model/Song.java
+++ src/main/java/com/example/demo/model/Song.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
+@JsonIgnoreProperties(value = {"album", "banner", "types", "playlists"}, allowSetters = true)
 public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,21 +27,17 @@ public class Song {
 
     private Long likes;
 
-    @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "albumId")
     private Album album;
 
-    @JsonIgnore
     @OneToOne(mappedBy = "song")
     private Banner banner;
 
-    @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "typeId")
     private Type types;
 
-    @JsonIgnore
     @ManyToMany(mappedBy = "songs")
     private List<Playlist> playlists;
 }
